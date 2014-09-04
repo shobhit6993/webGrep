c473292b2e235a5d31401334c53a81f363e47b54
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-09-05 02:06:45 +0530

    slabs eliminated. Everything working

--- createIndex.py
+++ createIndex.py
@@ -5,31 +5,25 @@ import tokenizer
 # 	s = fileObj.read()
 # 	return s.split()
 
-def increaseFileSize(slabSize):
+def increaseFileSize(chunkSize):
 	fileObj = open("PostingList","ab")
 	retVal = fileObj.tell()
-	print retVal
-	for i in xrange(0,slabSize):
+	for i in xrange(0,chunkSize):
 		fileObj.write('0')
 	fileObj.close()
 	return retVal
 
-def buildIndexMapAndPosList(keyList, indexMap, slabSize, lastOffset, posList):
+def buildPosList(keyList, posList):
 	i = 0
 	# posList = {}
-	for key in keyList:
-		if not indexMap.has_key(key):
-			indexMap[key] = lastOffset
-			lastOffset += slabSize
-			
+	for key in keyList:	
 		if not posList.has_key(key):
 			posList[keyList[i]] = [i]
 		else:
 			posList[keyList[i]].append(i)
 
 		i += 1
-	# print posList
-	return lastOffset
+
 
 def dumpIndexMap(indexMap):
 	cPickle.dump(indexMap, open("OffsetMap", "wb"))
@@ -37,44 +31,28 @@ def dumpIndexMap(indexMap):
 def readIndexMap():
 	return cPickle.load(open("OffsetMap", "rb"))
 
-def mergePostingList(postingListForABatch, indexMap, slabSize, bookKeeping, lastDump):
+def mergePostingList(postingListForABatch, indexMap, bookKeeping, lastDump):
 	fileObj = open("PostingList", "r+b")
 	for key in postingListForABatch:	
 		if lastDump.has_key(key):
 			try:
 				fileObj.seek(lastDump[key], 0)
-				book = fileObj.read(bookKeeping).split('$')
-				fileContent = fileObj.read(slabSize - bookKeeping)
-				postingListForAWord = cPickle.loads(fileContent)
-				postingListForAWord += postingListForABatch[key]
+				postingListForAWord = postingListForABatch[key]
 				dumpString = cPickle.dumps(postingListForAWord)
-				print str(book[1]) + " " + str(len(dumpString))
-				if len(dumpString) + bookKeeping  < slabSize:
-					fileObj.seek(lastDump[key], 0)
-					fileObj.write(str(-1) + "$" + str(len(dumpString)) + "$")
-					fileObj.seek(lastDump[key] + bookKeeping, 0)
-					fileObj.write(dumpString)
-				else:
-					print lastDump[key]
-					fileObj.seek(lastDump[key], 0)
-					lastDump[key] = increaseFileSize(slabSize)
-					print lastDump[key]
-					fileObj.write(str(lastDump[key]) + "$" + book[1] + "$")
-					postingListForAWord = postingListForABatch[key]
-					dumpString = cPickle.dumps(postingListForAWord)
-					fileObj.seek(lastDump[key], 0)
-					fileObj.write(str(-1) + "$" + str(len(dumpString)) + "$")
-					fileObj.seek(lastDump[key] + bookKeeping, 0)
-					fileObj.write(dumpString)
+				lastDump[key] = increaseFileSize(len(dumpString) + bookKeeping)
+				fileObj.write(str(lastDump[key]) + "$" )
+				fileObj.seek(lastDump[key], 0)
+				fileObj.write(str(-1) + "$")
+				fileObj.seek(lastDump[key] + bookKeeping, 0)
+				fileObj.write(dumpString)
 			except (EOFError,cPickle.UnpicklingError):
 				continue
 		else:
-			increaseFileSize(slabSize)
-			lastDump[key] = indexMap[key]
 			postingListForAWord = postingListForABatch[key]
 			dumpString = cPickle.dumps(postingListForAWord)
+			indexMap[key] = lastDump[key] = increaseFileSize(len(dumpString) + bookKeeping)
 			fileObj.seek(lastDump[key], 0)
-			fileObj.write(str(-1) + "$" + str(len(dumpString)) + "$")
+			fileObj.write(str(-1) + "$")
 			fileObj.seek(lastDump[key] + bookKeeping, 0)
 			fileObj.write(dumpString)
 
@@ -105,25 +83,20 @@ if __name__ == "__main__":
 	# Tokenising the files to get a keyList
 	keyList = []
 	indexMap = {}
-	bookKeeping = 50
-	slabSize = 10000 + bookKeeping
-	lastOffset = 0
+	bookKeeping = 15
 	lastDump = {}
-	for i in xrange(0, 2):
+	for i in xrange(0, 10):
 		postingListForABatch = {}
-		for j in xrange(0, 1):
-			# fileObj = open("./dataset/" + str(i) + "/" + str(j+50*i), "rb")
-			# keyList = tokenize(fileObj)`1
-			keyList = tokenizer.getTokenListFromHtml("./dataset/" + str(i) + "/" + str(j))
-			# fileObj.close()
+		for j in xrange(0, 1000):
+			keyList = tokenizer.getTokenListFromHtml("./dataset/" + str(0) + "/" + str(1000*i+j))
 			postingListForAFile = {}
-			lastOffset = buildIndexMapAndPosList(keyList, indexMap, slabSize, lastOffset, postingListForAFile)
+			buildPosList(keyList, postingListForAFile)
 			buildPostingListForABatch(postingListForABatch, postingListForAFile, i)
-			print "|keyList| = " + str(len(keyList))
-			print str(j) + "done"
-		mergePostingList(postingListForABatch, indexMap, slabSize, bookKeeping, lastDump)
+			# print "|keyList| = " + str(len(keyList))
+			print str(1000*i+j)
+		mergePostingList(postingListForABatch, indexMap, bookKeeping, lastDump)
 		#print postingListForABatch
 	# Dumping the index into a file
 	dumpIndexMap(indexMap)
 
-	#readPostingFile(indexMap)
+	readPostingFile(indexMap)
