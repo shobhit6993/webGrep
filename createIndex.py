e520fb3c819f8da217000d757ab8b69159b3a24c
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-09-04 18:31:09 +0530

    Initial slab model working. TOO slow

--- createIndex.py
+++ createIndex.py
@@ -4,34 +4,39 @@ def tokenize(fileObj):
 	s = fileObj.read()
 	return s.split()
 
+def increaseFileSize(slaSize):
+	fileObj = open("PostingList","ab+")
+	for i in xrange(0,slaSize):
+		fileObj.write('0')
+	fileObj.close()
+
 def buildIndexMap(keyList, indexMap, slaSize, lastOffset):
 	for key in keyList:
 		if not indexMap.has_key(key):
 			indexMap[key] = lastOffset
 			lastOffset += slaSize
+			increaseFileSize(slaSize)
+	return lastOffset
 
 def dumpIndexMap(indexMap):
-	cPickle.dump(indexMap, open("OffsetMap", "w"))
+	cPickle.dump(indexMap, open("OffsetMap", "wb"))
 
 def readIndexMap():
-	return cPickle.load(open("OffsetMap", "r"))
+	return cPickle.load(open("OffsetMap", "rb"))
 
 def createPostingListForAFile(keyList, indexMap):
 	posList = {}
-	for i in range( 0, len(keyList) ):
+	for i in xrange( 0, len(keyList) ):
 		if posList.has_key(keyList[i]):
 			posList[keyList[i]].append(i)
 		else:
 			posList[keyList[i]] = [i]
-	
 	return posList
 
 def mergePostingList(postingListForAFile, indexMap, docId, slaSize, dumpedOnce):
-	fileObj = open("PostingList", "a+")
+	fileObj = open("PostingList", "r+b")
 	for key in postingListForAFile:	
-		print dumpedOnce.has_key(key)
 		if dumpedOnce.has_key(key): 
-			print lskjdflksdlsdjf
 			fileObj.seek(indexMap[key], 0)
 			try:
 				postingListForAWord = cPickle.load(fileObj)
@@ -45,19 +50,13 @@ def mergePostingList(postingListForAFile, indexMap, docId, slaSize, dumpedOnce):
 			postingListForAWord = [[docId, postingListForAFile[key]]]
 			
 		fileObj.seek(indexMap[key], 0)
-		cPickle.dump(postingListForAWord, fileObj)
-		fileObj.seek(indexMap[key], 0)
-		try:
-			print cPickle.load(fileObj)
-		except(EOFError,cPickle.UnpicklingError):
-			continue
-
+		tempDump = cPickle.dumps(postingListForAWord)
+		fileObj.write(tempDump);
 	fileObj.close()
 
 def readPostingFile(indexMap):
-	fileObj = open("PostingList", "r")
+	fileObj = open("PostingList", "rb")
 	for key in indexMap:
-		#print key
 		fileObj.seek(indexMap[key], 0)
 		try:
 			print cPickle.load(fileObj)
@@ -66,52 +65,28 @@ def readPostingFile(indexMap):
 	
 	fileObj.close()	
 	
-def testPickle():
-	fileObj = open("in", "w+")
-	dic = {"aditya":True, "jha":1}
-	fileObj.seek(10, 0)
-	cPickle.dump(dic, fileObj)
-	dic = {}
-	fileObj.seek(10, 0)	
-	dic = cPickle.load(fileObj)	
-	fileObj.close()
-	print dic
-	
-	dic = {}
-	fileObj = open("in", "a+")
-	fileObj.seek(10, 0)	
-	dic = cPickle.load(fileObj)	
-	fileObj.close()
-	
-	print dic	
-		
-if __name__ == "__main__":
-#	testPickle()
 
+if __name__ == "__main__":
+	fileObj = open("PostingList","wb")
+	fileObj.close()
 	# Tokenising the files to get a keyList
 	keyList = []
 	indexMap = {}
-	slaSize = 100000
+	slaSize = 10000
 	lastOffset = 0
 	dumpedOnce = {}
-	for i in range(0, 1):
-		for j in range(0, 1):
+	for i in xrange(0, 1):
+		for j in xrange(0, 101):
 			fileObj = open("./" + str(i) + "/" + str(j), "rb")
 			keyList = tokenize(fileObj)
 			fileObj.close()
-			buildIndexMap(keyList, indexMap, slaSize, lastOffset)
+			lastOffset = buildIndexMap(keyList, indexMap, slaSize, lastOffset)
 			postingListForAFile = createPostingListForAFile(keyList, indexMap)
 			mergePostingList(postingListForAFile, indexMap, j, slaSize, dumpedOnce)
 			print "|keyList| = " + str(len(keyList))
+			print str(j) + "done"
 	
 	# Dumping the index into a file
 	dumpIndexMap(indexMap)
 
-	readPostingFile(indexMap)
-
-#	fileObj = open("PostingList", "rb")	
-#	postingList = {}
-#	for key in indexMap:
-#		postingListString = fileObj.read(slaSize)
-#		postingList[key] = cPickle.loads(postingListString)
-#	print postingList
+	# readPostingFile(indexMap)
