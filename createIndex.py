abe074c7c5e9d1df878b7ccae74e89770334501b
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-09-04 13:58:46 +0530

    Trying slab allocation. EOFerror

--- createIndex.py
+++ createIndex.py
@@ -1,64 +1,117 @@
-# populate keys of the dictionary
-# for a particular tokenlist
-def populateKeysForAFile(indexMap, tokenList):
-	for token in tokenList:
-		indexMap[token] = []
+import cPickle
 
-def getTokens(fileName):
-	f = open(fileName, "r")
-	s = f.read()
-	f.close()
+def tokenize(fileObj):
+	s = fileObj.read()
 	return s.split()
 
-def populateKeys(indexMap, fileList):
-	for fileName in fileList:
-		tokenList = getTokens(fileName)
-		populateKeysForAFile(indexMap, tokenList)
+def buildIndexMap(keyList, indexMap, slaSize, lastOffset):
+	for key in keyList:
+		if not indexMap.has_key(key):
+			indexMap[key] = lastOffset
+			lastOffset += slaSize
 
-def returnKeyList(indexMap):
-	keyList = []
-	for key, value in indexMap.iteritems():
-		keyList.append(key)
-	return keyList
+def dumpIndexMap(indexMap):
+	cPickle.dump(indexMap, open("OffsetMap", "w"))
 
-def returnBatchMap(start, keyList):
-	end = min(start+2000, len(keyList))
-	batchList = keyList[start:end]
-	return {word : "" for word in batchList}
+def readIndexMap():
+	return cPickle.load(open("OffsetMap", "r"))
 
-#TODO: have to tweak docId, right now its the filename
-def buildPostingList(batchMap, indexMap, fileList):
-	for fileName in fileList:
-		tokenList = getTokens(fileName)
-		postingMap = {}
-		pos = 0
-		for token in tokenList:
-			if batchMap.has_key(token):
-				if postingMap.has_key(token):
-					postingMap[token].append(pos)
-				else:
-					postingMap[token] = [pos]
-			pos += 1
-
-		for word in postingMap:
-			indexMap[word].append([fileName, postingMap[word]])
-
-if __name__ == "__main__":
-	indexMap = {}
-	fileList = []
-	for i in xrange(0,9999):
-		fileList.append("./dataset/0/"+str(i))# = ['dummy1.txt', 'dummy2.txt']
-	populateKeys(indexMap, fileList)
-	# print indexMap
-	# print returnVocab(indexMap);
-	keyList = returnKeyList(indexMap)
-
-	for start in xrange(0,len(keyList),2000):
-		batchMap = returnBatchMap(start, keyList)
-		buildPostingList(batchMap, indexMap, fileList)
+def createPostingListForAFile(keyList, indexMap):
+	posList = {}
+	for i in range( 0, len(keyList) ):
+		if posList.has_key(keyList[i]):
+			posList[keyList[i]].append(i)
+		else:
+			posList[keyList[i]] = [i]
 	
-	print len(indexMap)
+	return posList
 
+def mergePostingList(postingListForAFile, indexMap, docId, slaSize, dumpedOnce):
+	fileObj = open("PostingList", "a+")
+	for key in postingListForAFile:	
+		print dumpedOnce.has_key(key)
+		if dumpedOnce.has_key(key): 
+			print lskjdflksdlsdjf
+			fileObj.seek(indexMap[key], 0)
+			try:
+				postingListForAWord = cPickle.load(fileObj)
+				postingListForAWord.append([docId, postingListForAFile[key]])
+			except (EOFError,cPickle.UnpicklingError):
+				continue
+				
+			
+		else:
+			dumpedOnce[key] = ""
+			postingListForAWord = [[docId, postingListForAFile[key]]]
+			
+		fileObj.seek(indexMap[key], 0)
+		cPickle.dump(postingListForAWord, fileObj)
+		fileObj.seek(indexMap[key], 0)
+		try:
+			print cPickle.load(fileObj)
+		except(EOFError,cPickle.UnpicklingError):
+			continue
 
+	fileObj.close()
 
+def readPostingFile(indexMap):
+	fileObj = open("PostingList", "r")
+	for key in indexMap:
+		#print key
+		fileObj.seek(indexMap[key], 0)
+		try:
+			print cPickle.load(fileObj)
+		except(EOFError,cPickle.UnpicklingError):
+			continue
+	
+	fileObj.close()	
+	
+def testPickle():
+	fileObj = open("in", "w+")
+	dic = {"aditya":True, "jha":1}
+	fileObj.seek(10, 0)
+	cPickle.dump(dic, fileObj)
+	dic = {}
+	fileObj.seek(10, 0)	
+	dic = cPickle.load(fileObj)	
+	fileObj.close()
+	print dic
+	
+	dic = {}
+	fileObj = open("in", "a+")
+	fileObj.seek(10, 0)	
+	dic = cPickle.load(fileObj)	
+	fileObj.close()
+	
+	print dic	
+		
+if __name__ == "__main__":
+#	testPickle()
 
+	# Tokenising the files to get a keyList
+	keyList = []
+	indexMap = {}
+	slaSize = 100000
+	lastOffset = 0
+	dumpedOnce = {}
+	for i in range(0, 1):
+		for j in range(0, 1):
+			fileObj = open("./" + str(i) + "/" + str(j), "rb")
+			keyList = tokenize(fileObj)
+			fileObj.close()
+			buildIndexMap(keyList, indexMap, slaSize, lastOffset)
+			postingListForAFile = createPostingListForAFile(keyList, indexMap)
+			mergePostingList(postingListForAFile, indexMap, j, slaSize, dumpedOnce)
+			print "|keyList| = " + str(len(keyList))
+	
+	# Dumping the index into a file
+	dumpIndexMap(indexMap)
+
+	readPostingFile(indexMap)
+
+#	fileObj = open("PostingList", "rb")	
+#	postingList = {}
+#	for key in indexMap:
+#		postingListString = fileObj.read(slaSize)
+#		postingList[key] = cPickle.loads(postingListString)
+#	print postingList
