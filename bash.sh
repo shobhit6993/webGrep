b544995cfa8132cbaa8794a3766e536040ba3c56
Harshil Lodhi <lodhi.harshil@gmail.com>
2014-09-07 00:35:33 +0530

    bash script: script timeout handled with exit codes

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-for i in {0..1}
+for i in {13..15}
 do
   one=1
   ten=10
@@ -7,5 +7,10 @@ do
   y=$(($t * $ten))
   echo $x
   echo $y
-    timeout 1200 pypy createIndex.py $x $y
+    timeout 1500 pypy createIndex.py $x $y
+    rc=$?
+    if [[ $rc != 0 ]] ; then
+            echo $x >> kharab
+    fi
+
 done
