bfe0fa870593337b442fdf768e9d1a8271b35ead
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-09-14 22:27:10 +0530

    folder names added

--- configVars.py
+++ configVars.py
@@ -1,9 +1,9 @@
-# Basic = 0
-# StopWord removal = 1
-# stemming = 2
-# StopWord removal and stemming = 3
+# Basic = 0 							indexed_0_161_notStemmed_stopRetained/
+# StopWord removal = 1 					indexed_0_161_notStemmed_stopRemoved/
+# stemming = 2							indexed_0_161_stemmed_stopRetained/
+# StopWord removal and stemming = 3 	indexed_0_161_stemmed_stopRemoved/
 
 
-mode = 0
-indexFolder = "./indexed/"
+mode = 3
+indexFolder = "./indexed/indexed_0_161_stemmed_stopRemoved/"
 bookKeeping = 15
